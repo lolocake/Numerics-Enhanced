ab8755d4e76e26688f0ff1272e6f411a640f00a5
lolocake <130923850+lolocake@users.noreply.github.com>
2022-04-03 11:40:14 +0000

    New translations en.json (Urdu (Pakistan)) [ci skip] Signed-off-by: Björn Kimminich <bjoern.kimminich@gmx.de>

--- Numerics/Algebra/EigenCalculator.cs
+++ Numerics/Algebra/EigenCalculator.cs
@@ -50,4 +50,96 @@ namespace Orbifold.Numerics
 		/// <param name="a">The int to process.</param>
 		/// <param name="p">The int to process.</param>
 		/// <param name="q">The int to process.</param>
-		/// <returns>A Tuple&lt;double,double&gt;</retur
+		/// <returns>A Tuple&lt;double,double&gt;</returns>
+		private Tuple<double, double> Schur(RMatrix a, int p, int q)
+		{
+			double c, s = 0;
+			if(a[p, q] != 0) {
+				var tau = (a[q, q] - a[p, p]) / (2 * a[p, q]);
+				var t = 0d;
+				if(tau >= 0)
+					t = 1 / (tau + Math.Sqrt(tau + sqr(tau)));
+				else
+					t = -1 / (-tau + Math.Sqrt(1 + sqr(tau)));
+
+				c = 1 / Math.Sqrt(1 + sqr(t));
+				s = t * c;
+			} else {
+				c = 1;
+				s = 0;
+			}
+
+			return new Tuple<double, double>(c, s);
+		}
+
+		/// <summary>Sweeps.</summary>
+		/// <param name="p">The int to process.</param>
+		/// <param name="q">The int to process.</param>
+		private void Sweep(int p, int q)
+		{
+			// set jacobi rotation matrix
+			var cs = Schur(A, p, q);
+			double c = cs.Item1;
+			double s = cs.Item2;
+
+			if(c != 1 || s != 0) { // if rotation
+
+				/*************************
+                 * perform jacobi rotation
+                 *************************/
+				// calculating intermediate J.T * A
+				var pV = Vector.Create(A.ColumnCount, i => A[p, i] * c + A[q, i] * -s);
+				var qV = Vector.Create(A.ColumnCount, i => A[q, i] * c + A[p, i] * s);
+
+				// calculating A * J for inner p, q square
+				var App = pV[p] * c + pV[q] * -s;
+				var Apq = pV[q] * c + pV[p] * s;
+				var Aqq = qV[q] * c + qV[p] * s;
+
+				// fill in changes along box
+				pV[p] = App;
+				pV[q] = qV[p] = Apq;
+				qV[q] = Aqq;
+
+				/***************************
+                 * store accumulated results
+                 ***************************/
+				var pE = Vector.Create(V.RowCount, i => V[i, p] * c + V[i, q] * -s);
+				var qE = Vector.Create(V.RowCount, i => V[i, q] * c + V[i, p] * s);
+
+				/****************
+                 * matrix updates
+                 ****************/
+				// Update A
+				A[p, VectorType.Col] = pV;
+				A[p, VectorType.Row] = pV;
+				A[q, VectorType.Col] = qV;
+				A[q, VectorType.Row] = qV;
+
+				// Update V - not critical 
+				V[p, VectorType.Col] = pE;
+				V[q, VectorType.Col] = qE;
+			}
+		}
+
+		/// <summary>Parallels this object.</summary>
+		public void Parallel()
+		{
+			//Console.WriteLine("Starting new sweep!");
+			int N = A.ColumnCount;
+			// make even pairings
+			int n = N % 2 == 0 ? N : N + 1;
+
+			//varp round-iness of the robin
+			var queue = new Queue<int>(n - 1);
+
+			// fill queue
+			for(int i = 1; i < N; i++)
+				queue.Enqueue(i);
+			// add extra for odd pairings
+			if(N % 2 == 1)
+				queue.Enqueue(-1);
+
+			for(int i = 0; i < n - 1; i++) {
+				System.Threading.Tasks.Parallel.For(0, n / 2, j => {
+					int p, q, k = n - 1
