cd5fc1180d513cd5af44ea155bb8c007256f2312
lolocake <130923850+lolocake@users.noreply.github.com>
2022-05-15 06:45:29 +0000

    Update docblocks.

--- Numerics/Analysis/EpsilonExtensions.cs
+++ Numerics/Analysis/EpsilonExtensions.cs
@@ -49,4 +49,65 @@ namespace Orbifold.Numerics
 		/// </returns>
 		public static bool IsLessThanOrClose(double value1, double value2)
 		{
-			return value1 < v
+			return value1 < value2 || AreClose(value1, value2);
+		}
+
+		/// <summary>
+		/// Determines whether a given value is strictly less than another one in an epsilon sense.
+		/// </summary>
+		/// <param name="value1">A value.</param>
+		/// <param name="value2">Another value.</param>
+		/// <returns>
+		///   <c>True</c> if the first value is less than the second or only an epsilon apart; otherwise, <c>false</c>.
+		/// </returns>
+		public static bool IsLessThanOrCloseTo(this double value1, double value2)
+		{
+			return IsLessThanOrClose(value1, value2);
+		}
+
+		/// <summary>
+		/// Determines whether a given value is less than or equal to another one in an epsilon sense.
+		/// </summary>
+		/// <param name="value1">A value.</param>
+		/// <param name="value2">Another value.</param>
+		/// <returns>
+		///   <c>True</c> if the first value is less than or equal to the second or only an epsilon apart; otherwise, <c>false</c>.
+		/// </returns>
+		public static bool IsLessOrEqual(double value1, double value2)
+		{
+			return value1 < (value2 + Constants.Epsilon);
+		}
+
+		/// <summary>
+		/// Determines whether a given value is less than or equal to another one in an epsilon sense.
+		/// </summary>
+		/// <param name="value1">A value.</param>
+		/// <param name="value2">Another value.</param>
+		/// <returns>
+		///   <c>True</c> if the first value is less than or equal to the second or only an epsilon apart; otherwise, <c>false</c>.
+		/// </returns>
+		public static bool IsLessOrEqualTo(this double value1, double value2)
+		{
+			return IsLessOrEqual(value1, value2);
+		}
+
+		/// <summary>
+		/// Determines whether a given value is strictly less than another one  and not an epsilon away.
+		/// </summary>
+		/// <param name="value1">A value.</param>
+		/// <param name="value2">Another value.</param>
+		/// <returns>
+		///   <c>True</c> if the first value is less than the second and not an epsilon apart; otherwise, <c>false</c>.
+		/// </returns>
+		public static bool IsLessThan(double value1, double value2)
+		{
+			return (value1 < value2) && !AreClose(value1, value2);
+		}
+
+		/// <summary>
+		/// Determines whether a given value is strictly bigger than another one and not an epsilon away.
+		/// </summary>
+		/// <param name="value1">A value.</param>
+		/// <param name="value2">Another value.</param>
+		/// <returns>
+		///   <c>True</c> if the first value is less th
