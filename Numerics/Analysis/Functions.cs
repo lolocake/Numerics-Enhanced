63fdac1fc3825017b375a3dd7e6605c4f4d6e744
lolocake <130923850+lolocake@users.noreply.github.com>
2022-06-10 07:28:17 +0000

    Update stores.haml

--- Numerics/Analysis/Functions.cs
+++ Numerics/Analysis/Functions.cs
@@ -1019,4 +1019,75 @@ namespace Orbifold.Numerics
 				z = 8.0 / x;
 				y = z * z;
 				xx = x - 2.356194491;
-				ans1
+				ans1 = 1.0 + y * (0.183105e-2 + y * (-0.3516396496e-4
+				+ y * (0.2457520174e-5 + y * (-0.240337019e-6))));
+				ans2 = 0.04687499995 + y * (-0.2002690873e-3
+				+ y * (0.8449199096e-5 + y * (-0.88228987e-6
+				+ y * 0.105787412e-6)));
+				ans = Math.Sqrt(0.636619772 / x) * (Math.Sin(xx) * ans1 + z * Math.Cos(xx) * ans2);
+			}
+			return ans;
+		}
+
+		public static double BesselY0(double x)
+		{
+
+			double z;
+			double xx, y, ans, ans1, ans2;
+			if(x < 8.0) {
+				y = x * x;
+				ans1 = -2957821389.0 + y * (7062834065.0 + y * (-512359803.6
+				+ y * (10879881.29 + y * (-86327.92757 + y * 228.4622733))));
+				ans2 = 40076544269.0 + y * (745249964.8 + y * (7189466.438
+				+ y * (47447.26470 + y * (226.1030244 + y * 1.0))));
+				ans = (ans1 / ans2) + 0.636619772 * BesselJ0(x) * Math.Log(x);
+			} else {
+				z = 8.0 / x;
+				y = z * z;
+				xx = x - 0.785398164;
+				ans1 = 1.0 + y * (-0.1098628627e-2 + y * (0.2734510407e-4
+				+ y * (-0.2073370639e-5 + y * 0.2093887211e-6)));
+				ans2 = -0.1562499995e-1 + y * (0.1430488765e-3
+				+ y * (-0.6911147651e-5 + y * (0.7621095161e-6
+				+ y * (-0.934945152e-7))));
+				ans = Math.Sqrt(0.636619772 / x) * (Math.Sin(xx) * ans1 + z * Math.Cos(xx) * ans2);
+			}
+			return ans;
+		}
+
+		public static double BesselJ1(double x)
+		{
+			double ax, z;
+			double xx, y, ans, ans1, ans2;
+			if((ax = Math.Abs(x)) < 8.0) {
+				y = x * x;
+				ans1 = x * (72362614232.0 + y * (-7895059235.0 + y * (242396853.1
+				+ y * (-2972611.439 + y * (15704.48260 + y * (-30.16036606))))));
+				ans2 = 144725228442.0 + y * (2300535178.0 + y * (18583304.74
+				+ y * (99447.43394 + y * (376.9991397 + y * 1.0))));
+				ans = ans1 / ans2;
+			} else {
+				z = 8.0 / ax;
+				y = z * z;
+				xx = ax - 2.356194491;
+				ans1 = 1.0 + y * (0.183105e-2 + y * (-0.3516396496e-4
+				+ y * (0.2457520174e-5 + y * (-0.240337019e-6))));
+				ans2 = 0.04687499995 + y * (-0.2002690873e-3
+				+ y * (0.8449199096e-5 + y * (-0.88228987e-6
+				+ y * 0.105787412e-6)));
+				ans = Math.Sqrt(0.636619772 / ax) * (Math.Cos(xx) * ans1 - z * Math.Sin(xx) * ans2);
+				if(x < 0.0)
+					ans = -ans;
+			}
+			return ans;
+		}
+
+		public static double BesselJ0(double x)
+		{
+            
+			double ax, z;
+			double xx, y, ans, ans1, ans2;
+			if((ax = Math.Abs(x)) < 8.0) { 
+				y = x * x;
+				ans1 = 57568490574.0 + y * (-13362590354.0 + y * (651619640.7
+				+ 
