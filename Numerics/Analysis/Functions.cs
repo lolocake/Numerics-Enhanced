4c90886cba782265abae626151cdc5a6eadd73da
lolocake <130923850+lolocake@users.noreply.github.com>
2022-06-19 23:00:19 +0000

    travis-ci badge

--- Numerics/Analysis/Functions.cs
+++ Numerics/Analysis/Functions.cs
@@ -1345,4 +1345,83 @@ namespace Orbifold.Numerics
 		static double chebev(double a, double b, double[] c, int m, double x)
 		{
 
-			double d = 0.0, dd = 0
+			double d = 0.0, dd = 0.0, sv, y, y2;
+			int j;
+
+			if((x - a) * (x - b) > 0.0)
+				throw new Exception("Argument x in chebev is outside the computational range.");
+			y2 = 2.0 * (y = (2.0 * x - a - b) / (b - a));
+			for(j = m - 1; j >= 1; j--) {
+				sv = d;
+				d = y2 * d - dd + c[j];
+				dd = sv;
+			}
+			return y * d - dd + 0.5 * c[0];
+		}
+
+		static void beschb(double x, out double gam1, out double gam2, out double gampl, out  double gammi)
+		{
+			const int NUSE1 = 5;
+			const int NUSE2 = 5;
+			double xx;
+			var c1 = new[] {
+				-1.142022680371172e0, 6.516511267076e-3,
+				3.08709017308e-4, -3.470626964e-6, 6.943764e-9,
+				3.6780e-11, -1.36e-13
+			};
+			var c2 = new[] {
+				1.843740587300906e0, -0.076852840844786e0,
+				1.271927136655e-3, -4.971736704e-6, -3.3126120e-8,
+				2.42310e-10, -1.70e-13, -1.0e-15
+			};
+
+			xx = 8.0 * x * x - 1.0;
+			gam1 = chebev(-1.0, 1.0, c1, NUSE1, xx);
+			gam2 = chebev(-1.0, 1.0, c2, NUSE2, xx);
+			gampl = gam2 - x * (gam1);
+			gammi = gam2 + x * (gam1);
+		}
+
+		/// <summary>
+		/// Bessel J and Y for fractional order.
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="xnu"></param>
+		/// <param name="rj"></param>
+		/// <param name="ry"></param>
+		/// <param name="rjp"></param>
+		/// <param name="ryp"></param>
+		static void bessjy(double x, double xnu, out double rj, out  double ry, out  double rjp, out double ryp)
+		{
+			const double XMIN = 2.0;
+			const int MAXIT = 10000;
+			const double FPMIN = 1.0e-30;
+			const double EPS = 1.0e-10;
+			int i, isign, l, nl;
+			double a, b, br, bi, c, cr, ci, d, del, del1, den, di, dlr, dli, dr, e, f, fact, fact2,
+			fact3, ff, gam, gam1, gam2, gammi, gampl, h, p, pimu, pimu2, q, r, rjl,
+			rjl1, rjmu, rjp1, rjpl, rjtemp, ry1, rymu, rymup, rytemp, sum, sum1,
+			temp, w, x2, xi, xi2, xmu, xmu2;
+
+			if(x <= 0.0 || xnu < 0.0)
+				throw new Exception("Value and order should be positive.");
+			nl = (x < XMIN ? (int)(xnu + 0.5) : Math.Max(0, (int)(xnu - x + 1.5)));
+			xmu = xnu - nl;
+			xmu2 = xmu * xmu;
+			xi = 1.0 / x;
+			xi2 = 2.0 * xi;
+			w = xi2 / Math.PI;
+			isign = 1;
+			h = xnu * xi;
+			if(h < FPMIN)
+				h = FPMIN;
+			b = xi2 * xnu;
+			d = 0.0;
+			c = h;
+			for(i = 1; i <= MAXIT; i++) {
+				b += xi2;
+				d = b - d;
+				if(Math.Abs(d) < FPMIN)
+					d = FPMIN;
+				c = b - 1.0 / c;
+				if(Ma
