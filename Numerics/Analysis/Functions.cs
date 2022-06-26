7e9954aeaa05a14f8183010d46e3a36aeb491c6a
lolocake <130923850+lolocake@users.noreply.github.com>
2022-06-26 23:10:58 +0000

    Login fix

--- Numerics/Analysis/Functions.cs
+++ Numerics/Analysis/Functions.cs
@@ -1601,4 +1601,94 @@ namespace Orbifold.Numerics
 			for(l = nl; l >= 1; l--) {
 				ritemp = fact * ril + ripl;
 				fact -= xi;
-				ripl = fact * ritemp 
+				ripl = fact * ritemp + ril;
+				ril = ritemp;
+			}
+			f = ripl / ril;
+			if(x < XMIN) {
+				x2 = 0.5 * x;
+				pimu = Math.PI * xmu;
+				fact = (Math.Abs(pimu) < EPS ? 1.0 : pimu / Math.Sin(pimu));
+				d = -Math.Log(x2);
+				e = xmu * d;
+				fact2 = (Math.Abs(e) < EPS ? 1.0 : Math.Sinh(e) / e);
+				beschb(xmu, out gam1, out gam2, out gampl, out gammi);
+				ff = fact * (gam1 * Math.Cosh(e) + gam2 * fact2 * d);
+				sum = ff;
+				e = Math.Exp(e);
+				p = 0.5 * e / gampl;
+				q = 0.5 / (e * gammi);
+				c = 1.0;
+				d = x2 * x2;
+				sum1 = p;
+				for(i = 1; i <= MAXIT; i++) {
+					ff = (i * ff + p + q) / (i * i - xmu2);
+					c *= (d / i);
+					p /= (i - xmu);
+					q /= (i + xmu);
+					del = c * ff;
+					sum += del;
+					del1 = c * (p - i * ff);
+					sum1 += del1;
+					if(Math.Abs(del) < Math.Abs(sum) * EPS)
+						break;
+				}
+				if(i > MAXIT)
+					throw new Exception("bessk series fails to converge");
+				rkmu = sum;
+				rk1 = sum1 * xi2;
+			} else {
+				b = 2.0 * (1.0 + x);
+				d = 1.0 / b;
+				h = delh = d;
+				q1 = 0.0;
+				q2 = 1.0;
+				a1 = 0.25 - xmu2;
+				q = c = a1;
+				a = -a1;
+				s = 1.0 + q * delh;
+				for(i = 2; i <= MAXIT; i++) {
+					a -= 2 * (i - 1);
+					c = -a * c / i;
+					qnew = (q1 - b * q2) / a;
+					q1 = q2;
+					q2 = qnew;
+					q += c * qnew;
+					b += 2.0;
+					d = 1.0 / (b + a * d);
+					delh = (b * d - 1.0) * delh;
+					h += delh;
+					dels = q * delh;
+					s += dels;
+					if(Math.Abs(dels / s) < EPS)
+						break;
+				}
+				if(i > MAXIT)
+					throw new Exception("bessik: failure to converge in cf2");
+				h = a1 * h;
+				rkmu = Math.Sqrt(Math.PI / (2.0 * x)) * Math.Exp(-x) / s;
+				rk1 = rkmu * (xmu + x + 0.5 - h) * xi;
+			}
+			rkmup = xmu * xi * rkmu - rk1;
+			rimu = xi / (f * rkmu - rkmup);
+			ri = (rimu * ril1) / ril;
+			rip = (rimu * rip1) / ril;
+			for(i = 1; i <= nl; i++) {
+				rktemp = (xmu + i) * xi2 * rk1 + rkmu;
+				rkmu = rk1;
+				rk1 = rktemp;
+			}
+			rk = rkmu;
+			rkp = xnu * xi * rkmu - rk1;
+		}
+
+		static void sphbes(int n, double x, out double sj, out double sy, out double sjp, out double syp)
+		{
+
+			const double RTPIO2 = 1.2533141;
+			double factor, order, rj, rjp, ry, ryp;
+
+			if(n < 0 || x <= 0.0)
+				throw new Exception("bad arguments in sphbes");
+			order = n + 0.5;
+			bessjy(x, order, out rj, out ry, out r
