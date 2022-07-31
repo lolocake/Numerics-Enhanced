b6af8cfc49ffffb4c93b502c8e32ac5263df3d9d
lolocake <130923850+lolocake@users.noreply.github.com>
2022-07-31 19:25:48 +0000

    about section

--- Numerics/Data Generators/DataStore.cs
+++ Numerics/Data Generators/DataStore.cs
@@ -47,4 +47,17 @@ namespace Orbifold.Numerics
                                                 {"Back-to-back loan","an arrangement in which two companies in different countries borrow offsetting amounts in each other's currency and each repays it at a specified future date in its domestic currency. Such a loan, often between a company and its foreign subsidiary, eliminates the risk of loss from exchange rate fluctuations."},
                                                 {"Back pay","pay that is owed to an employee for work carried out before the current payment period and is either overdue or results from a backdated pay increase.  "},
                                                 {"Ballpark"," an informal term for a rough, estimated figure."},
-                                                {"Bill of sale","Formal legal document tha
+                                                {"Bill of sale","Formal legal document that conveys title to or interest in specific property from the seller to the buyer."},
+                                                {"Business  venture"," Taking financial risks in a commercial enterprise"},
+                                                {"Seed money"," a usually modest amount of money used to convert an idea into a viable business. Seed money is a form of venture capital."},
+                                                {"Settlement","the payment of a debt or charge."},
+                                                {"Sole proprietorship","Business legal structure in which one individual owns the business."},
+                                                {"Venture funding","the round of funding for a new company that follows seed funding provided by venture capitalists.    "},
+                                                {"Viral marketing","the rapid spread of a message about a new product or service in a similar way to the spread of a virus  "},
+                                                {"Vulture capitalist","a venture capitalist who structures deals on behalf of an entrepreneur in such a way that the investors benefit rather than the entrepreneur"},
+                                                {"Wallet technology","a software package providing digital wallets or purses on the computers of merchants and customers to facilitate payment by digital cash"},
+                                                {"Whistleblowing"," speaking out to the media or the public on malpractice, misconduct, corruption, or mismanagement witnessed in an organization"},
+                                                {"Withholding tax","the money that an employer pays directly to the U.S. government as a payment of the income tax on the employee "},
+                                                {"Patent","a type of copyright granted as a fixed-term monopoly to an inventor by the state to prevent others copying an invention, or improvement  of a product or process"},
+                                                {"Point of purchase","the place at which a product is purchased by the customer. The point of sale can be a retail outlet, a display case, or even a  legal business relationship of two or more people who share responsibilities, resources, profits and liabilities."},
+                                                {"Price floor","The lowest amount a business owner can charge for a
