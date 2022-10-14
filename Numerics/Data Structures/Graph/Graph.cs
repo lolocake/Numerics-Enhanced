bbb0c6f19e43a6fb3f4289aabe0e1ac304b99192
lolocake <130923850+lolocake@users.noreply.github.com>
2022-10-14 22:03:45 +0000

    Sixth commit

--- Numerics/Data Structures/Graph/Graph.cs
+++ Numerics/Data Structures/Graph/Graph.cs
@@ -1,2 +1,74 @@
 ﻿using System;
-using System.Co
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Orbifold.Numerics
+{
+	/// <summary>
+	/// Base graph class for the various incarnations in the graph analysis.
+	/// </summary>
+	/// <remarks>
+	/// <list type="bullet">
+	/// <item>
+	/// <description>The graph is directed by default (<code lang="C#">IsDirected =
+	/// true</code>)</description></item>
+	/// <item>
+	/// <description>The adjacency structure is not centralized but resides in the
+	/// Outgoing and Incoming collection attached to the
+	/// Nodes.</description></item></list>
+	/// </remarks>
+	/// <typeparam name="TNode">The data type of the node which should be an
+	/// implementation of the <see cref="INode{TNode,TLink}">INode{TNode,TLink}</see>
+	/// interface and have a parameterless constructor.</typeparam>
+	/// <typeparam name="TLink">The data type of the edge which should be an
+	/// implementation of the <see cref="IEdge{TNode,TLink}">IEdge{TNode,TLink}</see>
+	/// interface and have a parameterless constructor.</typeparam>
+	public class Graph<TNode, TLink>
+		where TNode : class, INode<TNode, TLink>, new()
+		where TLink : class, IEdge<TNode, TLink>, new()
+	{
+		/// <summary>
+		/// The <see cref="IsDirected"/> field.
+		/// </summary>
+		private bool isDirected;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Graph{TNode,TLink}"/> class. 
+		/// </summary>
+		public Graph()
+		{
+			this.Nodes = new List<TNode>();
+			this.Edges = new List<TLink>();
+			this.isDirected = true;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Graph{TNode,TLink}"/> class.
+		/// </summary>
+		/// <param name="graph">The graph content to start with. Note that references will be added, not clones.</param>
+		public Graph(Graph<TNode, TLink> graph)
+			: this()
+		{
+			graph.Nodes.ForEach(n => this.Nodes.Add(n));
+			graph.Edges.ForEach(l => this.Edges.Add(l));
+		}
+
+		/// <summary>
+		/// Gets whether this graph is connected.
+		/// See also this article;  http://en.wikipedia.org/wiki/Connected_graph. 
+		/// </summary>
+		/// <remarks>
+		/// A graph is connected if every two vertices are connected by a path. A connected
+		/// graph has only one component.
+		/// </remarks>
+		/// <value>
+		/// <c>true</c> if this instance is connected; otherwise, <c>false</c>.
+		/// </value>
+		public bool IsConnected
+		{
+			get
+			{
+				return this.GetConnectedComponents().Count() == 1;
+			}
+		
