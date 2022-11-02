8cb23ef76b0bc8372b857ac086a45db14afb316e
lolocake <130923850+lolocake@users.noreply.github.com>
2022-11-02 14:23:02 +0000

    sched: Remove stale power aware scheduling remnants and dysfunctional knobs

--- Numerics/Data Structures/Heap.cs
+++ Numerics/Data Structures/Heap.cs
@@ -30,3 +30,72 @@ namespace Orbifold.Numerics
 		/// <summary>
 		/// Initializes a new instance of the <see cref="Heap&lt;TData&gt;"/> class.
 		/// </summary>
+		/// <param name="type">The type.</param>
+		/// <param name="capacity">The capacity.</param>
+		public Heap(OrderType type, int capacity)
+			: this(type, capacity, Comparer<TData>.Default)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Heap&lt;TData&gt;"/> class.
+		/// </summary>
+		/// <param name="type">The type.</param>
+		/// <param name="comparer">The comparer.</param>
+		public Heap(OrderType type, Comparison<TData> comparer)
+			: this(type, new ComparisonComparer<TData>(comparer))
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Heap&lt;TData&gt;"/> class.
+		/// </summary>
+		/// <param name="type">The type.</param>
+		/// <param name="capacity">The capacity.</param>
+		/// <param name="comparer">The comparer.</param>
+		public Heap(OrderType type, int capacity, Comparison<TData> comparer)
+			: this(type, capacity, new ComparisonComparer<TData>(comparer))
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Heap&lt;TData&gt;"/> class.
+		/// </summary>
+		/// <param name="type">The type.</param>
+		/// <param name="comparer">The comparer.</param>
+		public Heap(OrderType type, IComparer<TData> comparer)
+		{
+			if((type != OrderType.Ascending) && (type != OrderType.Descending)) {
+				throw new ArgumentOutOfRangeException("type");
+			}
+			this.orderingType = type;
+			this.internalList = new List<TData> { default(TData) };
+			this.comparer = type == OrderType.Ascending ? comparer : new ReverseComparer<TData>(comparer);
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Heap&lt;TData&gt;"/> class.
+		/// </summary>
+		/// <param name="type">The type.</param>
+		/// <param name="capacity">The capacity.</param>
+		/// <param name="comparer">The comparer.</param>
+		public Heap(OrderType type, int capacity, IComparer<TData> comparer)
+		{
+			if((type != OrderType.Ascending) && (type != OrderType.Descending)) {
+				throw new ArgumentOutOfRangeException("type");
+			}
+			this.orderingType = type;
+			this.internalList = new List<TData>(capacity) { default(TData) };
+			this.comparer = type == OrderType.Ascending ? comparer : new ReverseComparer<TData>(comparer);
+		}
+
+		/// <summary>
+		/// Gets the number of items in this heap structure.
+		/// </summary>
+		public int Count {
+			get {
+				return this.internalList.Count - 1;
+			}
+		}
+
+		/// <summary>
