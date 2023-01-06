1492dd7214101d2d65ee79946a606eee878bee33
lolocake <130923850+lolocake@users.noreply.github.com>
2023-01-06 21:43:37 +0000

    squashing bugs

--- Numerics/Functional Extensions/Composition.cs
+++ Numerics/Functional Extensions/Composition.cs
@@ -418,4 +418,44 @@ namespace Orbifold.Numerics
 		/// <param name="func1">The first functional.</param>
 		/// <param name="func2">The second functional.</param>
 		/// <param name="func3">The third functional.</param>
-		/// <param name="func4">The fourth functional.</para
+		/// <param name="func4">The fourth functional.</param>
+		/// <returns></returns>
+		public static Func<TDomain1, TDomain2, TDomain3, TEndResult> Compose<TDomain1, TDomain2, TDomain3, TIR1, TIR2, TIR3, TEndResult>(
+		  this Func<TDomain1, TDomain2, TDomain3, TIR1> func1, Func<TIR1, TIR2> func2, Func<TIR2, TIR3> func3, Func<TIR3, TEndResult> func4)
+		{
+			return (p1, p2, p3) => func4(func3(func2(func1(p1, p2, p3))));
+		}
+
+		/// <summary>
+		/// Composition of the given functionals.
+		/// </summary>
+		/// <typeparam name="TDomain1">The data type of the first parameter.</typeparam>
+		/// <typeparam name="TDomain2">The data type of the second parameter.</typeparam>
+		/// <typeparam name="TDomain3">The data type of the thrid parameter.</typeparam>
+		/// <typeparam name="TDomain4">The data type of the fourth parameter.</typeparam>
+		/// <typeparam name="TIR1">The type of the I r1.</typeparam>
+		/// <typeparam name="TIR2">The type of the I r2.</typeparam>
+		/// <typeparam name="TIR3">The type of the I r3.</typeparam>
+		/// <typeparam name="TEndResult">The type of the end result.</typeparam>
+		/// <param name="func1">The first functional.</param>
+		/// <param name="func2">The second functional.</param>
+		/// <param name="func3">The third functional.</param>
+		/// <param name="func4">The fourth functional.</param>
+		/// <returns></returns>
+		public static Func<TDomain1, TDomain2, TDomain3, TDomain4, TEndResult> Compose<TDomain1, TDomain2, TDomain3, TDomain4, TIR1, TIR2, TIR3, TEndResult>(
+		  this Func<TDomain1, TDomain2, TDomain3, TDomain4, TIR1> func1, Func<TIR1, TIR2> func2, Func<TIR2, TIR3> func3, Func<TIR3, TEndResult> func4)
+		{
+			return (p1, p2, p3, p4) => func4(func3(func2(func1(p1, p2, p3, p4))));
+		}
+
+		/// <summary>
+		/// Composition of the given functionals.
+		/// </summary>
+		/// <typeparam name="TDomain1">The data type of the first parameter.</typeparam>
+		/// <typeparam name="TDomain2">The data type of the second parameter.</typeparam>
+		/// <typeparam name="TDomain3">The data type of the thrid parameter.</typeparam>
+		/// <typeparam name="TDomain4">The data type of the fourth parameter.</typeparam>
+		/// <typeparam name="TDomain5">The data type of the fifth parameter.</typeparam>
+		/// <typeparam name="TIR1">The type of the I r1.</typeparam>
+		/// <typeparam name="TIR2">The type of the I r2.</typeparam>
+		/// <typeparam name="TIR3">The
