f3bd9f264f08dec9b3caac9dadd947d3994deca8
lolocake <130923850+lolocake@users.noreply.github.com>
2023-02-03 10:38:51 +0000

    Copy edit.

--- Numerics/Functional Extensions/Composition.cs
+++ Numerics/Functional Extensions/Composition.cs
@@ -785,4 +785,48 @@ namespace Orbifold.Numerics
 		/// <typeparam name="TIR2">The type of the I r2.</typeparam>
 		/// <typeparam name="TIR3">The type of the I r3.</typeparam>
 		/// <typeparam name="TIR4">The type of the I r4.</typeparam>
-		/// <typeparam name="TEndResult">The type of the end result.</type
+		/// <typeparam name="TEndResult">The type of the end result.</typeparam>
+		/// <param name="func1">The first functional.</param>
+		/// <param name="func2">The second functional.</param>
+		/// <param name="func3">The third functional.</param>
+		/// <param name="func4">The fourth functional.</param>
+		/// <param name="func5">The fifth functional.</param>
+		/// <returns></returns>
+		public static Func<TDomain1, TDomain2, TDomain3, TDomain4, TDomain5, TDomain6, TDomain7, TDomain8, TDomain9, TEndResult> Compose<TDomain1, TDomain2, TDomain3, TDomain4, TDomain5, TDomain6, TDomain7, TDomain8, TDomain9, TIR1, TIR2, TIR3, TIR4, TEndResult>(
+		  this Func<TDomain1, TDomain2, TDomain3, TDomain4, TDomain5, TDomain6, TDomain7, TDomain8, TDomain9, TIR1> func1, Func<TIR1, TIR2> func2, Func<TIR2, TIR3> func3, Func<TIR3, TIR4> func4, Func<TIR4, TEndResult> func5)
+		{
+			return (p1, p2, p3, p4, p5, p6, p7, p8, p9) => func5(func4(func3(func2(func1(p1, p2, p3, p4, p5, p6, p7, p8, p9)))));
+		}
+
+		/// <summary>
+		/// Composition of the given functionals.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the source.</typeparam>
+		/// <typeparam name="TIntermediateResult">The type of the intermediate result.</typeparam>
+		/// <param name="func1">The first functional.</param>
+		/// <param name="action">The action.</param>
+		/// <returns></returns>
+		public static Action<TSource> Compose<TSource, TIntermediateResult>(
+		  this Func<TSource, TIntermediateResult> func1, Action<TIntermediateResult> action)
+		{
+			return sourceParam => action(func1(sourceParam));
+		}
+
+		/// <summary>
+		/// Composition of the given functionals.
+		/// </summary>
+		/// <typeparam name="TDomain1">The data type of the first parameter.</typeparam>
+		/// <typeparam name="TDomain2">The data type of the second parameter.</typeparam>
+		/// <typeparam name="TIntermediateResult">The type of the intermediate result.</typeparam>
+		/// <param name="func1">The first functional.</param>
+		/// <param name="action">The action.</param>
+		/// <returns></returns>
+		public static Action<TDomain1, TDomain2> Compose<TDomain1, TDomain2, TIntermediateResult>(
+		  this Func<TDomain1, TDomain2, TIntermediateResult> func1, Action<TIntermediateResult> action)
+		{
+			return (p1, p2) => action(func1(p1, p2));
+		}
+
+		/// <summary>
+		/// Composition of the given functionals.
+		/// </summ
