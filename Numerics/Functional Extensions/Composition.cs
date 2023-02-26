3a66288ade695db802710a47321f6e73c549b5d6
lolocake <130923850+lolocake@users.noreply.github.com>
2023-02-26 09:28:08 +0000

    Make tests pass.

--- Numerics/Functional Extensions/Composition.cs
+++ Numerics/Functional Extensions/Composition.cs
@@ -1380,4 +1380,43 @@ namespace Orbifold.Numerics
 		}
 
 		/// <summary>
-		/// Comp
+		/// Composition of the given functionals.
+		/// </summary>
+		/// <typeparam name="TDomain1">The data type of the first parameter.</typeparam>
+		/// <typeparam name="TDomain2">The data type of the second parameter.</typeparam>
+		/// <typeparam name="TDomain3">The data type of the thrid parameter.</typeparam>
+		/// <typeparam name="TIR1">The type of the I r1.</typeparam>
+		/// <typeparam name="TIR2">The type of the I r2.</typeparam>
+		/// <typeparam name="TIR3">The type of the I r3.</typeparam>
+		/// <typeparam name="TIR4">The type of the I r4.</typeparam>
+		/// <param name="func1">The first functional.</param>
+		/// <param name="func2">The second functional.</param>
+		/// <param name="func3">The third functional.</param>
+		/// <param name="func4">The fourth functional.</param>
+		/// <param name="action">The action.</param>
+		/// <returns></returns>
+		public static Action<TDomain1, TDomain2, TDomain3> Compose<TDomain1, TDomain2, TDomain3, TIR1, TIR2, TIR3, TIR4>(
+		  this Func<TDomain1, TDomain2, TDomain3, TIR1> func1, Func<TIR1, TIR2> func2, Func<TIR2, TIR3> func3, Func<TIR3, TIR4> func4, Action<TIR4> action)
+		{
+			return (p1, p2, p3) => action(func4(func3(func2(func1(p1, p2, p3)))));
+		}
+
+		/// <summary>
+		/// Composition of the given functionals.
+		/// </summary>
+		/// <typeparam name="TDomain1">The data type of the first parameter.</typeparam>
+		/// <typeparam name="TDomain2">The data type of the second parameter.</typeparam>
+		/// <typeparam name="TDomain3">The data type of the thrid parameter.</typeparam>
+		/// <typeparam name="TDomain4">The data type of the fourth parameter.</typeparam>
+		/// <typeparam name="TIR1">The type of the I r1.</typeparam>
+		/// <typeparam name="TIR2">The type of the I r2.</typeparam>
+		/// <typeparam name="TIR3">The type of the I r3.</typeparam>
+		/// <typeparam name="TIR4">The type of the I r4.</typeparam>
+		/// <param name="func1">The first functional.</param>
+		/// <param name="func2">The second functional.</param>
+		/// <param name="func3">The third functional.</param>
+		/// <param name="func4">The fourth functional.</param>
+		/// <param name="action">The action.</param>
+		/// <returns></returns>
+		public static Action<TDomain1, TDomain2, TDomain3, TDomain4> Compose<TDomain1, TDomain2, TDomain3, TDomain4, TIR1, TIR2, TIR3, TIR4>(
+		  this Func<TDomain1, TDomain2, TDomain3, TDomain4, TIR1> f
