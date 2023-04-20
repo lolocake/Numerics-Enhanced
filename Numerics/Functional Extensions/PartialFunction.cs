31cff182c1e54f0dfe2e0b181b483a5fcc7e02b0
lolocake <130923850+lolocake@users.noreply.github.com>
2023-04-20 11:07:40 +0000

    updating docs

--- Numerics/Functional Extensions/PartialFunction.cs
+++ Numerics/Functional Extensions/PartialFunction.cs
@@ -390,4 +390,37 @@ namespace Orbifold.Numerics
 		/// <typeparam name="TDomain3">The data type of the thrid parameter.</typeparam>
 		/// <typeparam name="TDomain4">The data type of the fourth parameter.</typeparam>
 		/// <typeparam name="TDomain5">The data type of the fifth parameter.</typeparam>
-		/// <typeparam name="
+		/// <typeparam name="TDomain6">The data type of the sixth parameter.</typeparam>
+		/// <typeparam name="TDomain7">The data type of the seventh parameter.</typeparam>
+		/// <typeparam name="TRange">The type of the range.</typeparam>
+		/// <param name="function">The function.</param>
+		/// <param name="arg1">The first argument.</param>
+		/// <param name="arg2">The second argument.</param>
+		/// <param name="arg3">The third argument.</param>
+		/// <param name="arg4">The fourth argument.</param>
+		/// <param name="arg5">The fifth argument.</param>
+		/// <param name="arg6">The sixth argument.</param>
+		/// <returns></returns>
+		public static Func<TDomain7, TRange> Partial<TDomain1, TDomain2, TDomain3, TDomain4, TDomain5, TDomain6, TDomain7, TRange>(Func<TDomain1, TDomain2, TDomain3, TDomain4, TDomain5, TDomain6, TDomain7, TRange> function, TDomain1 arg1, TDomain2 arg2, TDomain3 arg3, TDomain4 arg4, TDomain5 arg5, TDomain6 arg6)
+		{
+			return arg7 => function(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+		}
+
+		/// <summary>
+		/// Partial application of the given functional.
+		/// </summary>
+		/// <typeparam name="TDomain1">The data type of the first parameter.</typeparam>
+		/// <typeparam name="TDomain2">The data type of the second parameter.</typeparam>
+		/// <typeparam name="TDomain3">The data type of the thrid parameter.</typeparam>
+		/// <typeparam name="TDomain4">The data type of the fourth parameter.</typeparam>
+		/// <typeparam name="TDomain5">The data type of the fifth parameter.</typeparam>
+		/// <typeparam name="TDomain6">The data type of the sixth parameter.</typeparam>
+		/// <typeparam name="TDomain7">The data type of the seventh parameter.</typeparam>
+		/// <typeparam name="TDomain8">The data type of the eigth parameter.</typeparam>
+		/// <typeparam name="TRange">The type of the range.</typeparam>
+		/// <param name="function">The function.</param>
+		/// <param name="arg1">The first argument.</param>
+		/// <returns></returns>
+		public static Func<TDomain2, TDomain3, TDomain4, TDomain5, TDomain6, TDomain7, TDomain8, TRange> Partial<TDomain1, TDomain2, TDomain3, TDomain4, TDomain5, TDomain6, TDomain7, TDomain8, TRange>(Func<TDomain1, TDomain2, TDomain3, TDomain4, TDomain5, TDomain6, TDomain7, TDomain8, TRange> function, TDomain1 arg1)
+		{
+			return (arg2, arg3, arg4, arg5, arg6, a
