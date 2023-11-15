d47cc2eafc0f7cbb78b3a0eb17af8ed6dbe52ef2
lolocake <130923850+lolocake@users.noreply.github.com>
2023-11-15 17:20:11 +0000

    cfq-iosched: fix incorrect filing of rt async cfqq

--- Numerics/Number/Numbers.cs
+++ Numerics/Number/Numbers.cs
@@ -36,4 +36,74 @@ namespace Orbifold.Numerics
 		/// <returns>True if and only if it is an odd number.</returns>
 		public static bool IsOdd(this int number)
 		{
-			return (number &
+			return (number & 0x1) == 0x1;
+		}
+
+		/// <summary>
+		/// Find out whether the provided 32 bit integer is a perfect power of two.
+		/// </summary>
+		/// <param name="number">The number to very whether it's a power of two.</param>
+		/// <returns>True if and only if it is a power of two.</returns>
+		public static bool IsPowerOfTwo(this int number)
+		{
+			return number > 0 && (number & (number - 1)) == 0x0;
+		}
+
+		/// <summary>
+		/// Find out whether the provided 64 bit integer is a perfect power of two.
+		/// </summary>
+		/// <param name="number">The number to very whether it's a power of two.</param>
+		/// <returns>True if and only if it is a power of two.</returns>
+		public static bool IsPowerOfTwo(this long number)
+		{
+			return number > 0 && (number & (number - 1)) == 0x0;
+		}
+
+		/// <summary>
+		/// Find the closest perfect power of two that is larger or equal to the provided
+		/// 32 bit integer.
+		/// </summary>
+		/// <param name="number">The number of which to find the closest upper power of two.</param>
+		/// <returns>A power of two.</returns>
+		public static int CeilingToPowerOfTwo(this int number)
+		{
+			if (number == int.MinValue) return 0;
+
+			const int MaxPowerOfTwo = 0x40000000;
+			if (number > MaxPowerOfTwo) throw new ArgumentOutOfRangeException("number");
+
+			number--;
+			number |= number >> 1;
+			number |= number >> 2;
+			number |= number >> 4;
+			number |= number >> 8;
+			number |= number >> 16;
+			return number + 1;
+		}
+
+		/// <summary>
+		/// Raises 2 to the provided integer exponent (0 &lt;= exponent &lt; 31).
+		/// </summary>
+		/// <param name="exponent">The exponent to raise 2 up to.</param>
+		/// <returns>2 ^ exponent.</returns>
+		/// <exception cref="ArgumentOutOfRangeException"/>
+		public static int PowerOfTwo(this int exponent)
+		{
+			if (exponent < 0 || exponent >= 31) throw new ArgumentOutOfRangeException("exponent");
+			return 1 << exponent;
+		}
+
+		/// <summary>
+		/// Returns the greatest common divisor of the given list.
+		/// </summary>
+		public static int GreatestCommonDivisor(IList<int> integers)
+		{
+			if (null == integers) throw new ArgumentNullException("integers");
+			if (integers.Count == 0) return 0;
+			var gcd = System.Math.Abs(integers[0]);
+			for (var i = 1; (i < integers.Count) && (gcd > 1); i++) gcd = GreatestCommonDivisor(gcd, integers[i]);
+			return gcd;
+		}
+
+		/// <summary>
+		/// R
