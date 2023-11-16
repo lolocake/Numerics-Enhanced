07660d4bd0eff1414c6ce6702933a1fb8f5175d4
lolocake <130923850+lolocake@users.noreply.github.com>
2023-11-16 16:28:32 +0000

    Internal change.

--- Numerics/PortableAdditions/MatrixUtil.cs
+++ Numerics/PortableAdditions/MatrixUtil.cs
@@ -12,4 +12,64 @@ namespace Orbifold.Numerics
         /// <param name="matrix"> The Matrix with which to transform the Rect. </param>
         internal static void TransformRect(ref Rect rect, ref Matrix matrix)
         {
- 
+            if (rect.IsEmpty)
+            {
+                return;
+            }
+
+            MatrixTypes matrixType = matrix._type;
+
+            // If the matrix is identity, don't worry.
+            if (matrixType == MatrixTypes.TRANSFORM_IS_IDENTITY)
+            {
+                return;
+            }
+
+            // Scaling
+            if (0 != (matrixType & MatrixTypes.TRANSFORM_IS_SCALING))
+            {
+                rect._x *= matrix._m11;
+                rect._y *= matrix._m22;
+                rect._width *= matrix._m11;
+                rect._height *= matrix._m22;
+
+                // Ensure the width is always positive.  For example, if there was a reflection about the
+                // y axis followed by a translation into the visual area, the width could be negative.
+                if (rect._width < 0.0)
+                {
+                    rect._x += rect._width;
+                    rect._width = -rect._width;
+                }
+
+                // Ensure the height is always positive.  For example, if there was a reflection about the
+                // x axis followed by a translation into the visual area, the height could be negative.
+                if (rect._height < 0.0)
+                {
+                    rect._y += rect._height;
+                    rect._height = -rect._height;
+                }
+            }
+
+            // Translation
+            if (0 != (matrixType & MatrixTypes.TRANSFORM_IS_TRANSLATION))
+            {
+                // X
+                rect._x += matrix._offsetX;
+
+                // Y
+                rect._y += matrix._offsetY;
+            }
+
+            if (matrixType == MatrixTypes.TRANSFORM_IS_UNKNOWN)
+            {
+                // Al Bunny implementation.
+                Point point0 = matrix.Transform(rect.TopLeft);
+                Point point1 = matrix.Transform(rect.TopRight);
+                Point point2 = matrix.Transform(rect.BottomRight);
+                Point point3 = matrix.Transform(rect.BottomLeft);
+
+                // Width and height is always positive here.
+                rect._x = Math.Min(Math.Min(point0.X, point1.X), Math.Min(point2.X, point3.X));
+                rect._y = Math.Min(Math.Min(point0.Y, point1.Y), Math.Min(point2.Y, point3.Y));
+
+                rect._width = Math.Max(Math.Max(p
