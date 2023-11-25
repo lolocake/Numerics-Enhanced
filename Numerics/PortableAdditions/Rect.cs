ac33a840d542fab41ddcf0316587957c020260fd
lolocake <130923850+lolocake@users.noreply.github.com>
2023-11-25 11:05:20 +0000

    Updated my site

--- Numerics/PortableAdditions/Rect.cs
+++ Numerics/PortableAdditions/Rect.cs
@@ -69,4 +69,91 @@ namespace Orbifold.Numerics
                     Vector2D vector)
             : this(point, vector+point)
         {
-   
+        }
+
+        /// <summary>
+        /// Constructor which sets the initial values to bound the (0,0) point and the point 
+        /// that results from (0,0) + size. 
+        /// </summary>
+        public Rect(Size size)
+        {
+            if (size.IsEmpty)
+            {
+                this = s_empty;
+            }
+            else
+            {
+                _x = _y = 0;
+                _width = size.Width;
+                _height = size.Height;
+            }
+        }
+
+        #endregion Constructors
+
+        #region Statics
+
+        /// <summary> 
+        /// Empty - a static property which provides an Empty rectangle.  X and Y are positive-infinity
+        /// and Width and Height are negative infinity.  This is the only situation where Width or
+        /// Height can be negative.
+        /// </summary> 
+        public static Rect Empty
+        {
+            get
+            {
+                return s_empty;
+            }
+        }
+
+        #endregion Statics
+
+        #region Public Properties
+
+        /// <summary>
+        /// IsEmpty - this returns true if this rect is the Empty rectangle. 
+        /// Note: If width or height are 0 this Rectangle still contains a 0 or 1 dimensional set
+        /// of points, so this method should not be used to check for 0 area.
+        /// </summary>
+        public bool IsEmpty
+        {
+            get
+            {
+                // The funny width and height tests are to handle NaNs
+                Debug.Assert((!(_width < 0) && !(_height < 0)) || (this == Empty));
+
+                return _width < 0;
+            }
+        }
+
+        /// <summary> 
+        /// Location - The Point representing the origin of the Rectangle 
+        /// </summary>
+        public Point Location
+        {
+            get
+            {
+                return new Point(_x, _y);
+            }
+            set
+            {
+                if (IsEmpty)
+                {
+                    throw new System.InvalidOperationException("Cannot modify empty rectangle.");
+                }
+
+                _x = value._x;
+                _y = value._y;
+            }
+        }
+
+        /// <summary> 
+        /// Size - The Size representing the area of the Rectangle
+        /// </summary>
+        public Size Size
+        {
+            get
+            {
+                if (IsEmpty)
+                    return Size.Empty;
+ 
