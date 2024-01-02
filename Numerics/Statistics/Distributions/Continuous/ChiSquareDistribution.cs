931f15bb3b0a4e41e0a895a98f84d1effd0cb243
lolocake <130923850+lolocake@users.noreply.github.com>
2024-01-02 11:13:34 +0000

    update by script

--- Numerics/Statistics/Distributions/Continuous/ChiSquareDistribution.cs
+++ Numerics/Statistics/Distributions/Continuous/ChiSquareDistribution.cs
@@ -38,4 +38,82 @@ namespace Orbifold.Numerics
         public int DegreesOfFreedom { get; private set; }
 
         /// <summary>
-        ///     Upper limit of a random
+        ///     Upper limit of a random variable with this probability distribution.
+        /// </summary>
+        public override double Maximum
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        ///     Gets the mean for this distribution.
+        /// </summary>
+        /// <remarks>
+        ///     The χ² distribution mean is the number of degrees of freedom.
+        /// </remarks>
+        public override double Mean
+        {
+            get
+            {
+                return this.DegreesOfFreedom;
+            }
+            set
+            {
+                this.DegreesOfFreedom = (int)value;
+            }
+        }
+
+        /// <summary>
+        ///     The value separating the lower half part from the upper half part of a random variable with this probability
+        ///     distribution.
+        /// </summary>
+        public override double Median
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        ///     Gets the variance for this distribution.
+        /// </summary>
+        /// <remarks>
+        ///     The χ² distribution variance is twice its degrees of freedom.
+        /// </remarks>
+        public override double Variance
+        {
+            get
+            {
+                return 2.0 * this.DegreesOfFreedom;
+            }
+        }
+
+        /// <summary>
+        ///     Measure of the asymmetry of this probability distribution.
+        /// </summary>
+        public override double Skewness
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        ///     Gets the complementary cumulative distribution function
+        ///     (ccdf) for the χ² distribution evaluated at point <c>x</c>.
+        ///     This function is also known as the Survival function.
+        /// </summary>
+        /// <remarks>
+        ///     <para>
+        ///         The Complementary Cumulative Distribution Function (CCDF) is
+        ///         the complement of the Cumulative Distribution Function, or 1
+        ///         minus the CDF.
+        ///     </para>
+        ///     <para>
+        ///         The χ² complementary distribution function is defined in terms of the
+        ///         <see cref="Gamma
