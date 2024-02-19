a9857d7b9dd3cef29098b304e374e7befa9ae543
lolocake <130923850+lolocake@users.noreply.github.com>
2024-02-19 06:58:19 +0000

    ignoring stuff

--- Numerics.Test/DataStructures/GraphAnalysisTests.cs
+++ Numerics.Test/DataStructures/GraphAnalysisTests.cs
@@ -83,4 +83,87 @@ namespace Orbifold.Numerics.Tests.DataStructures
 				// making it undirected means the spanning tree should reach all the nodes since the graph is connected
 				g.IsDirected = false;
 
-				var tree =
+				var tree = g.PrimsSpanningTree(g.FindNode(1), true);
+				Assert.AreEqual(g.Nodes.Count, tree.Nodes.Count, "Seems the spanning tree is not so spanning things.");
+				tree.IsDirected = true;
+				Assert.IsFalse(tree.Nodes.Any(n => n.Parents.Count() > 1), "Some nodes have more than one parent.");
+			}
+
+		}
+
+		#endregion
+
+		#region Kruskal
+
+		[Test]
+#if SILVERLIGHT
+        [Tag("Graph Analysis")]
+#else
+		[Category("Graph Analysis")]
+		#endif
+		public void KruskalTest1()
+		{
+			var g = GraphExtensions.Parse(new[] { "1,2", "1,3", "2,4", "3,4", "4,5" });
+			var tree = g.KruskalsSpanningTree(true);
+			Assert.AreNotEqual(g.Edges.Count, tree.Edges.Count, "One edge should have been removed to have a spanning tree.");
+			var cycles = tree.FindCycles();
+			Assert.AreEqual(0, cycles.Count, "There shouldn't be any cycles now.");
+		}
+
+		[Test]
+#if SILVERLIGHT
+        [Tag("Graph Analysis")]
+#else
+		[Category("Graph Analysis")]
+		#endif
+		public void KruskalTest2()
+		{
+			// Penrose diagram
+			var g = GraphExtensions.Parse(new[] { "1,3", "2,1", "2,4", "2,5", "3,4", "3,6", "4,5", "4,6", "7,5", "7,6" });
+
+			// by specifyin 'true' the tree will be a flow
+			var tree = g.KruskalsSpanningTree(true);
+			var cycles = tree.FindCycles();
+			Assert.AreEqual(0, cycles.Count, "There shouldn't be any cycles now.");
+			Assert.IsFalse(tree.Nodes.Any(n => n.Parents.Count() > 1), "Some nodes have more than one parent.");
+
+		}
+
+		/// <summary>
+		/// Kruskal over some random graphs.
+		/// Note that Kruskal works only with directed graphs.
+		/// </summary>
+		[Test]
+#if SILVERLIGHT
+        [Tag("Graph Analysis")]
+#else
+		[Category("Graph Analysis")]
+		#endif
+		public void KruskalRandomTest()
+		{
+
+			var howmany = Rand.Next(5, 13);
+
+			// we'll do the same test with different random graphs a few times
+			for(var i = 0; i < howmany; i++) {
+				var size = Rand.Next(10, 40);
+				var g = GraphExtensions.CreateRandomConnectedGraph(size);
+
+				var tree = g.KruskalsSpanningTree(true);
+				
+				Assert.IsTrue(tree.IsAcyclic, "There shouldn't be any cycles now.");
+				Assert.AreEqual(g.Nodes.Count, tree.Nodes.Count, "Seems the spanning tree is not so spanning things.");
+				tree.IsDirected = true;
+				Assert.IsFalse(tree.Nodes.Any(n => n.Parents.Count() > 1), "Some nodes have more than one parent.");
+			}
+
+		}
+
+		#endregion
+
+
+		#region Cycles
+
+		[Test]
+#if SILVERLIGHT
+        [Tag("Graph Analysi
