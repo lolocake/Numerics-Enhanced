1ace81a82e209172412acab86f061d768f86a13e
lolocake <130923850+lolocake@users.noreply.github.com>
2024-04-02 21:53:56 +0000

    install instructions

--- Numerics.Test/Utils/Iris.cs
+++ Numerics.Test/Utils/Iris.cs
@@ -67,4 +67,42 @@ namespace Orbifold.Numerics.Tests
 				new Iris { SepalLength = 4.9m, SepalWidth = 3.1m, PetalLength = 1.5m, PetalWidth = 0.1m, Class = "Iris-setosa" }, 
 				new Iris { SepalLength = 5m, SepalWidth = 3.2m, PetalLength = 1.2m, PetalWidth = 0.2m, Class = "Iris-setosa" }, 
 				new Iris { SepalLength = 5.5m, SepalWidth = 3.5m, PetalLength = 1.3m, PetalWidth = 0.2m, Class = "Iris-setosa" }, 
-				new I
+				new Iris { SepalLength = 4.9m, SepalWidth = 3.1m, PetalLength = 1.5m, PetalWidth = 0.1m, Class = "Iris-setosa" }, 
+				new Iris { SepalLength = 4.4m, SepalWidth = 3m, PetalLength = 1.3m, PetalWidth = 0.2m, Class = "Iris-setosa" }, 
+				new Iris { SepalLength = 5.1m, SepalWidth = 3.4m, PetalLength = 1.5m, PetalWidth = 0.2m, Class = "Iris-setosa" }, 
+				new Iris { SepalLength = 5m, SepalWidth = 3.5m, PetalLength = 1.3m, PetalWidth = 0.3m, Class = "Iris-setosa" }, 
+				new Iris { SepalLength = 4.5m, SepalWidth = 2.3m, PetalLength = 1.3m, PetalWidth = 0.3m, Class = "Iris-setosa" }, 
+				new Iris { SepalLength = 4.4m, SepalWidth = 3.2m, PetalLength = 1.3m, PetalWidth = 0.2m, Class = "Iris-setosa" }, 
+				new Iris { SepalLength = 5m, SepalWidth = 3.5m, PetalLength = 1.6m, PetalWidth = 0.6m, Class = "Iris-setosa" }, 
+				new Iris { SepalLength = 5.1m, SepalWidth = 3.8m, PetalLength = 1.9m, PetalWidth = 0.4m, Class = "Iris-setosa" }, 
+				new Iris { SepalLength = 4.8m, SepalWidth = 3m, PetalLength = 1.4m, PetalWidth = 0.3m, Class = "Iris-setosa" }, 
+				new Iris { SepalLength = 5.1m, SepalWidth = 3.8m, PetalLength = 1.6m, PetalWidth = 0.2m, Class = "Iris-setosa" }, 
+				new Iris { SepalLength = 4.6m, SepalWidth = 3.2m, PetalLength = 1.4m, PetalWidth = 0.2m, Class = "Iris-setosa" }, 
+				new Iris { SepalLength = 5.3m, SepalWidth = 3.7m, PetalLength = 1.5m, PetalWidth = 0.2m, Class = "Iris-setosa" }, 
+				new Iris { SepalLength = 5m, SepalWidth = 3.3m, PetalLength = 1.4m, PetalWidth = 0.2m, Class = "Iris-setosa" }, 
+				new Iris { SepalLength = 7m, SepalWidth = 3.2m, PetalLength = 4.7m, PetalWidth = 1.4m, Class = "Iris-versicolor" }, 
+				new Iris {
+					SepalLength = 6.4m,
+					SepalWidth = 3.2m,
+					PetalLength = 4.5m,
+					PetalWidth = 1.5m,
+					Class = "Iris-versicolor"
+				}, 
+				new Iris {
+					SepalLength = 6.9m,
+					SepalWidth = 3.1m,
+					PetalLength = 4.9m,
+					PetalWidth = 1.5m,
+					Class = "Iris-versicolor"
+				}, 
+				new Iris { SepalLength = 5.5m, SepalWidth = 2.3m, PetalLength = 4m, PetalWidth = 1.3m, Class = "Iris-versicolor" }, 
+				new Iris {
+					SepalLength = 6.5m,
+					SepalWidth = 2.8m,
+					PetalLength = 4.6m,
+					PetalWidth = 1.5m,
+					Class = "Iris-versicolor"
+				}, 
+				new Iris {
+					SepalLength = 5.7m,
+					SepalWi
